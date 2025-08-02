dd2c36008d701e54e295dcb111225e6802283aec
cajzc <cajzc.dev@gmail.com>
2025-08-01 17:51:27 -0700

    Fix authorization bug

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router';
+import { AuthorizationProvider } from '../lib/AuthorizationProvider';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="index" />
-    </Stack>
+    <AuthorizationProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="index" />
+      </Stack>
+    </AuthorizationProvider>
   );
 } 
