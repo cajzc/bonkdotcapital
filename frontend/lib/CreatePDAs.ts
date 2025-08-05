97073bcecc8c3f49c5736e826f60ee8e5b1851e1
cajzc <cajzc.dev@gmail.com>
2025-08-04 19:32:13 -0700

    Add helpers to make all pdas

---
+++ frontend/lib/CreatePDAs.ts
@@ -0,0 +1,25 @@
+import { PublicKey } from "@solana/web3.js";
+
+export function createLoanOfferPDA(lender: PublicKey, tokenMint: PublicKey, programId: PublicKey): PublicKey {
+    const [loanOfferPda] = PublicKey.findProgramAddressSync(
+        [Buffer.from('loan_offer'), lender.toBuffer(), tokenMint.toBuffer()],
+        programId
+    );
+    return loanOfferPda;
+}
+
+export function createLoanPDA(borrower: PublicKey, loanOfferPda: PublicKey, programId: PublicKey): PublicKey {
+    const [loanPda] = PublicKey.findProgramAddressSync(
+        [Buffer.from('loan'), loanOfferPda.toBuffer(), borrower.toBuffer()],
+        programId
+    );
+    return loanPda;
+}
+
+export function createVaultPDA(loanOfferPda: PublicKey, programId: PublicKey): PublicKey {
+    const [vaultPda] = PublicKey.findProgramAddressSync(
+        [Buffer.from('vault'), loanOfferPda.toBuffer()],
+        programId
+    );
+    return vaultPda;
+}
