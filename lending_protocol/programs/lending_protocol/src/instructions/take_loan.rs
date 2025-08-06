fc7b48b733695b322a6ccfecf8a71bb7d63a9915
cajzc <cajzc.dev@gmail.com>
2025-08-05 22:52:44 -0700

    Update take loan instruction to create accounts open loan, collateral vault, and optionally borrower ata

--- lending_protocol/programs/lending_protocol/src/instructions/take_loan.rs
+++ lending_protocol/programs/lending_protocol/src/instructions/take_loan.rs
@@ -1,6 +1,6 @@
 use crate::{
     errors::Errors,
-    state::{borrower_profile::BorrowerProfile, loan::{Loan, LoanInfo}, obligation::Obligation},
+    state::{borrower_profile::BorrowerProfile, loan::{OpenLoan, LoanInfo}, collateral::CollateralVault},
 };
 use anchor_lang::prelude::*;
 use anchor_spl::{
@@ -10,119 +10,110 @@ use anchor_spl::{
 };
 
 
-pub fn take_loan(ctx: Context<TakeLoan>, amount: u64) -> Result<()> {
-    require!(amount > 0, Errors::InvalidAmount);
+pub fn take_loan(ctx: Context<TakeLoan>, collateral_amount: u64) -> Result<()> {
+    let clock = Clock::get()?;
+    require!(
+        ctx.accounts.borrower_token_account.amount < collateral_amount,
+        Errors::CollateralNotEnough
+    );
+    require!(ctx.accounts.loan_info.is_active, Errors::OfferNotActive);
+    // FIXME: always passes
+    require!(
+        clock.unix_timestamp <= ctx.accounts.loan_info.duration_seconds as i64 + clock.unix_timestamp,
+        Errors::LoanOfferExpired
+    );
+    require!(
+      !ctx.accounts.loan_info.is_active,
+      Errors::LoanAlreadyExists
+    );
 
-    let collateral = &mut ctx.accounts.collateral;
-    collateral.borrower = ctx.accounts.borrower.key();
-    collateral.collateral_token_mint = ctx.accounts.token_mint.key();
-    collateral.collateral_account = ctx.accounts.collateral_vault.key();
-    collateral.bump = ctx.bumps.collateral;
-    collateral.deposited_amount = collateral
-        .deposited_amount
-        .checked_add(amount)
-        .ok_or(Errors::MathOverflow)?;
+    // Update loan info
+    let loan_info = &mut ctx.accounts.loan_info;
+    loan_info.is_active = true;
 
+    // Open the loan
+    let open_loan = &mut ctx.accounts.open_loan;
+    open_loan.loan_info = ctx.accounts.loan_info.key();
+    open_loan.borrower = ctx.accounts.borrower.key();
+    open_loan.principal = ctx.accounts.loan_info.amount;
+    open_loan.start_time = ctx.accounts.clock.unix_timestamp;
+    open_loan.repay_by_time = ctx.accounts.clock.unix_timestamp + ctx.accounts.loan_info.duration_seconds;
+    open_loan.is_repaid = false;
+    open_loan.bump = ctx.bumps.open_loan;
+
+    // Deposit the collateral
     let cpi_accounts = Transfer {
         from: ctx.accounts.borrower_token_account.to_account_info(),
         to: ctx.accounts.collateral_vault.to_account_info(),
         authority: ctx.accounts.borrower.to_account_info(),
     };
     let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
-    token::transfer(cpi_ctx, amount)?;
+    anchor_spl::token::transfer(cpi_ctx, collateral_amount)?;
+
+    // Create the collateral vault
+    let collateral_vault = &mut ctx.accounts.collateral_vault;
+    collateral_vault.borrower = ctx.accounts.borrower.key();
+    collateral_vault.token_mint= ctx.accounts.token_mint.key();
+    collateral_vault.loan_info = ctx.accounts.loan_info.key();
+    collateral_vault.amount = collateral_amount;
+    collateral_vault.is_active = true;
+    collateral_vault.bump = ctx.bumps.collateral_vault;
 
     msg!(
-        "Borrower {} deposited {} collateral",
-        collateral.borrower,
-        amount
+        "Borrower {} deposited {} {} tokens as collateral",
+        collateral_vault.borrower,
+        collateral_vault.amount,
+        collateral_vault.token_mint
     );
     
-    
-    let loan_offer = &mut ctx.accounts.loan_offer;
-    let loan = &mut ctx.accounts.loan;
-    let clock = Clock::get()?;
-    let obligation = &mut ctx.accounts.obligation;
-
-    require!(loan_offer.is_active, Errors::OfferNotActive);
-    // Removed borrower_profile score check
-    require!(
-        clock.unix_timestamp <= loan_offer.duration_seconds as i64 + clock.unix_timestamp,
-        Errors::LoanOfferExpired
-    );
-    require!(
-      !obligation.loan_active,
-      Errors::LoanAlreadyExists
-    );
-
-    obligation.loan_active = true;
-
-    // Initialize Loan Account
-    loan.offer = loan_offer.key();
-    loan.borrower = ctx.accounts.borrower.key();
-    loan.principal = loan_offer.amount;
-    loan.start_time = clock.unix_timestamp;
-    loan.repay_by_time = clock.unix_timestamp + loan_offer.duration_seconds as i64;
-    loan.is_repaid = false;
-    loan.bump = ctx.bumps.loan;
-
     // Transfer loan amount from vault to borrower
     let seeds = &[
-        b"loan_offer",
-        loan_offer.lender.as_ref(),
-        loan_offer.token_mint.as_ref(),
-        &[loan_offer.bump],
+        b"vault",
+        ctx.accounts.loan_info.key().as_ref(),
+        &[ctx.bumps.loan_info],
     ];
     let signer = &[&seeds[..]];
     let cpi_accounts = Transfer {
         from: ctx.accounts.vault.to_account_info(),
         to: ctx.accounts.borrower_token_account.to_account_info(),
-        authority: loan_offer.to_account_info(),
+        authority: ctx.accounts.loan_info.to_account_info(),
     };
     let cpi_program = ctx.accounts.token_program.to_account_info();
     let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
-    anchor_spl::token::transfer(cpi_ctx, loan_offer.amount)?;
-
-    // Mark loan offer as inactive
-    loan_offer.is_active = false;
+    anchor_spl::token::transfer(cpi_ctx, ctx.accounts.loan_info.amount)?;
 
     msg!(
-        "Loan accepted: Borrower {} received {} from offer {}",
+        "Loan taken: Borrower {} received {} from offer {}",
         ctx.accounts.borrower.key(),
-        loan_offer.amount,
-        loan_offer.key()
+        ctx.accounts.loan_info.amount,
+        ctx.accounts.loan_info.key()
     );
 
     Ok(())
 }
 
 #[derive(Accounts)]
-#[instruction(bump: u8)]
 pub struct TakeLoan<'info> {
-    #[account(
-        mut,
-        seeds = [b"loan_offer", loan_offer.lender.as_ref(), token_mint.key().as_ref()],
-        bump = loan_offer.bump,
-        has_one = token_mint,
-        has_one = vault
-    )]
-    pub loan_offer: Account<'info, LoanOffer>,
-
     #[account(
         init,
         payer = borrower,
         space = 8 + 32 + 32 + 8 + 8 + 8 + 8 + 1 + 1,
-        seeds = [b"loan", loan_offer.key().as_ref(), borrower.key().as_ref()],
+        seeds = [b"open_loan", loan_info.key().as_ref(), borrower.key().as_ref()],
         bump
     )]
-    pub loan: Account<'info, Loan>,
+    /// State of the loan taken by a borrower
+    pub open_loan: Account<'info, OpenLoan>,
 
     #[account(
-        mut,
-        seeds = [b"obligation", borrower.key().as_ref()],
-        bump = obligation.bump,
+        init,
+        payer = borrower,
+        space = 8 + 32 + 32 + 8 + 1 + 1,
+        seeds = [b"collateral_vault", loan_info.key().as_ref(), borrower.key().as_ref()],
+        bump,
         has_one = borrower
     )]
-    pub obligation: Account<'info, Obligation>,
+    /// Stores the collateral token and metadata
+    pub collateral_vault: Account<'info, CollateralVault>,
 
     #[account(
         init_if_needed,
@@ -132,203 +123,32 @@ pub struct TakeLoan<'info> {
         associated_token::token_program = token_program,
     )]
     pub borrower_token_account: InterfaceAccount<'info, TokenAccount>,
-
+    
     #[account(
         mut,
-        token::mint = token_mint,
-        token::authority = loan_offer
+        token::mint = loan_info.loan_token_mint,
+        token::authority = loan_info, 
+        seeds = [b"vault", loan_info.key().as_ref()],
+        bump
     )]
+    /// Holds the tokens sent out for a loan prior to a second party borrowing
     pub vault: InterfaceAccount<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        seeds = [b"loan_info", lender.key().as_ref(), token_mint.key().as_ref()],
+        bump
+    )]
+    /// Stores metadata about the loan offer
+    pub loan_info: Account<'info, LoanInfo>,
 
     #[account(mut)]
     pub borrower: Signer<'info>,
-
+    pub lender: AccountInfo<'info>,
     pub token_mint: InterfaceAccount<'info, Mint>,
     pub token_program: Interface<'info, TokenInterface>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
     pub clock: Sysvar<'info, Clock>,
-}
-
-pub fn pay_loan(ctx:Context<PayLoan>) -> Result<()>{
-    let loan = &mut ctx.accounts.loan;
-    let clock = Clock::get()?;
-    let obligation = &mut ctx.accounts.obligation;
-
-    // Ensure loan isn't already repaid
-    require!(!loan.is_repaid, Errors::LoanAlreadyRepaid);
-
-    //Calculate time elapse in seconds
-    let time_elapsed = (clock.unix_timestamp - loan.start_time) as u64;
-    let duration_seconds = (loan.repay_by_time - loan.start_time) as u64;
-    let effective_time = time_elapsed.min(duration_seconds);
-
-    //Constants
-    const SECONDS_PER_YEAR: u64 = 31_536_000;
-
-    //Calculate interest using u128 to avoid overflow
-    let principal = loan.principal as u128;
-    let interest_rate_bps = ctx.accounts.loan_offer.interest_rate_bps as u128;
-    let effective_time = effective_time as u128;
-    let denominator = (SECONDS_PER_YEAR as u128) * 10_000 as u128;
-    let interest = (principal * interest_rate_bps * effective_time) / denominator;
-    let interest = interest as u64;
-
-    // Total repayment amount
-    let total_amount = loan.principal.checked_add(interest).ok_or(Errors::MathOverflow)?;
-
-    // Transfer principal + interest from borrower to lender
-    let cpi_accounts = Transfer {
-        from: ctx.accounts.borrower_token_account.to_account_info(),
-        to: ctx.accounts.lender_token_account.to_account_info(),
-        authority: ctx.accounts.borrower.to_account_info(),
-    };
-    let cpi_program = ctx.accounts.token_program.to_account_info();
-    let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-    anchor_spl::token::transfer(cpi_ctx, total_amount)?;
-
-    // Mark loan as repaid
-    loan.is_repaid = true;
-    obligation.loan_active = false;
-
-    //Return collateral to the borrower
-    let amount_to_return = obligation.deposited_amount;
-
-    if amount_to_return > 0 {
-        let seeds = &[
-            b"obligation",
-            ctx.accounts.borrower.key.as_ref(),
-            &[obligation.bump],
-        ];
-
-        let signer = &[&seeds[..]];
-
-        let cpi_accounts = Transfer {
-            from: ctx.accounts.collateral_vault.to_account_info(),
-            to: ctx.accounts.borrower_token_account.to_account_info(),
-            authority: obligation.to_account_info(),
-        };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
-
-        anchor_spl::token::transfer(cpi_ctx, amount_to_return)?;
-        msg!("Returned {} collateral to borrower", amount_to_return);
-    }
-
-    msg!(
-        "Loan repaid: Borrower {} paid {} (principal: {}, interest: {}) to lender {}",
-        ctx.accounts.borrower.key(),
-        total_amount,
-        loan.principal,
-        interest,
-        ctx.accounts.loan_offer.lender
-    );
-
-    Ok(())
-}
-
-
-#[derive(Accounts)]
-pub struct PayLoan<'info>{
-    #[account(
-        mut,
-        seeds = [b"loan", loan_offer.key().as_ref(), borrower.key().as_ref()],
-        bump = loan.bump,
-        constraint = loan.offer == loan_offer.key(),
-        has_one = borrower
-    )]
-    pub loan: Account<'info, Loan>,
-
-    #[account(
-        seeds = [b"loan_offer", loan_offer.lender.as_ref(), token_mint.key().as_ref()],
-        bump = loan_offer.bump,
-        has_one = token_mint,
-    )]
-    pub loan_offer: Account<'info, LoanOffer>,
-
-    #[account(
-        mut,
-        seeds = [b"obligation", borrower.key().as_ref()],
-        bump = obligation.bump,
-        has_one = borrower
-    )]
-    pub obligation: Account<'info, Obligation>,
-
-    #[account(
-        mut,
-        seeds = [b"collateral_vault", borrower.key().as_ref(), token_mint.key().as_ref()],
-        bump,
-        token::mint = token_mint,
-        token::authority = obligation
-    )]
-    pub collateral_vault: InterfaceAccount<'info, TokenAccount>,
-
-    #[account(
-        mut,
-        token::mint = token_mint,
-        token::authority = borrower
-    )]
-    pub borrower_token_account: InterfaceAccount<'info, TokenAccount>,
-
-    #[account(
-        mut,
-        token::mint = token_mint,
-        token::authority = loan_offer.lender
-    )]
-    pub lender_token_account: InterfaceAccount<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub borrower: Signer<'info>,
-    pub token_mint: InterfaceAccount<'info, Mint>,
-    pub token_program: Interface<'info, TokenInterface>,
-    pub clock: Sysvar<'info, Clock>,
-}
-
-use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
-
-use crate::errors::Errors;
-use crate::instructions::{AcceptLoan, TakeLoan};
-use crate::state::obligation::Obligation;
-
-pub fn deposit_collateral(ctx: Context<TakeLoan>, amount: u64) -> Result<()> {
-    
-
-    Ok(())
-}
-
-#[derive(Accounts)]
-#[instruction(amount: u64, bump: u8)]
-pub struct DepositCollateral<'info> {
-    #[account(
-     init_if_needed,
-     payer = borrower,
-     space = 8 + 32 + 32 + 32 + 8 + 1 + 1,
-     seeds = [b"obligation", borrower.key().as_ref()],
-     bump
-    )]
-    pub obligation: Account<'info, Obligation>,
-    #[account(
-     mut,
-     token::mint = token_mint,
-     token::authority = borrower
-    )]
-    pub borrower_token_account: Account<'info, TokenAccount>,
-    #[account(
-     init_if_needed,
-     payer = borrower,
-     token::mint = token_mint,
-     token::authority = obligation,
-     seeds = [b"collateral_vault", borrower.key().as_ref(), token_mint.key().as_ref()],
-     bump
-    )]
-    pub collateral_vault: Account<'info, TokenAccount>,
-
-    pub token_mint: Account<'info, Mint>,
-    #[account(mut)]
-    pub borrower: Signer<'info>,
-    pub token_program: Program<'info, Token>,
-    pub system_program: Program<'info, System>,
-    pub rent: Sysvar<'info, Rent>,
 }
